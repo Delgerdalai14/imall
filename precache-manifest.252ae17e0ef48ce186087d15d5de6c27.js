c5bc537db09bf206f4c66e51aeefdce74e8e7dd2
Delgerdalai14 <bdeegii@gmail.com>
2020-06-23 23:32:20 +0800

    Updates

---
+++ precache-manifest.252ae17e0ef48ce186087d15d5de6c27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a6f69c9a0e6c7945f429b2fbb38456",
+    "url": "/imall/index.html"
+  },
+  {
+    "revision": "76bb8d986b3b868ab953",
+    "url": "/imall/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f80fa944033227628da",
+    "url": "/imall/static/js/2.bfa71efd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imall/static/js/2.bfa71efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76bb8d986b3b868ab953",
+    "url": "/imall/static/js/main.076b8d55.chunk.js"
+  },
+  {
+    "revision": "fe89e39b3f1b6c22d779",
+    "url": "/imall/static/js/runtime-main.e223d577.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imall/static/media/logo.5d5d9eef.svg"
+  }
+]);
